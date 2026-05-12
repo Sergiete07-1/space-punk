a436bfb2dd3f3b181cf5c2d20163e5e22d45c9b2
Sergio Ruiz <sergio.ruizluengo@gmail.com>
2026-05-12 12:58:00 +0200

    feat: movimiento del personaje con WASD

--- src/scenes/GameScene.js
+++ src/scenes/GameScene.js
@@ -6,13 +6,36 @@ export default class GameScene extends Phaser.Scene {
 
   create() {
     const graphics = this.add.graphics();
+    
 
     for(let fila = 0; fila < 18; fila++){
       for(let columna = 0; columna < 25; columna++){
         graphics.fillStyle(0x228B22);
         graphics.fillRect(columna * 32, fila * 32, 32, 32);
       }
+      
     }
-    this.add.rectangle(400, 300, 32, 32, 0xff0000);
-  }
+  this.player = this.add.rectangle(400, 300, 32, 32, 0xff0000);  
+  this.keys = this.input.keyboard.addKeys('W,A,S,D');
+}
+update() {
+const velocidad = 3;
+
+if (this.keys.W.isDown) {
+  this.player.y -= velocidad;
+}
+
+
+if (this.keys.A.isDown) {
+  this.player.x -= velocidad;
+}
+
+if (this.keys.S.isDown) {
+  this.player.y += velocidad;
+}
+
+if (this.keys.D.isDown) {
+  this.player.x += velocidad;
+}
+}
 }
