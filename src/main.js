4d4023a026253bcd0b832765f4d7e84fb04e8bcb
Sergio Ruiz <sergio.ruizluengo@gmail.com>
2026-05-15 12:53:58 +0200

    feat: colisiones con obstáculos y límites del mundo

--- src/main.js
+++ src/main.js
@@ -6,7 +6,11 @@ const config = {
   width: 800,
   height: 600,
   backgroundColor: '#2d2d2d',
-    scene: [GameScene]
-  };
+  scene: [GameScene],
+  physics: {
+    default: 'arcade'
+  }
+};
 
 new Phaser.Game(config);
+
--- src/scenes/GameScene.js
+++ src/scenes/GameScene.js
@@ -5,48 +5,39 @@ export default class GameScene extends Phaser.Scene {
   }
 
   create() {
-    const graphics = this.add.graphics();
-    
+  const graphics = this.add.graphics();
 
-    for(let fila = 0; fila < 54; fila++){
-      for(let columna = 0; columna < 75; columna++){
-        graphics.fillStyle(0x228B22);
-        graphics.fillRect(columna * 32, fila * 32, 32, 32);
-      }
-      
+  for (let fila = 0; fila < 54; fila++) {
+    for (let columna = 0; columna < 75; columna++) {
+      graphics.fillStyle(0x228B22);
+      graphics.fillRect(columna * 32, fila * 32, 32, 32);
     }
-  this.player = this.add.rectangle(400, 300, 32, 32, 0xff0000);  
+  }
+
+  this.player = this.add.rectangle(400, 300, 32, 32, 0xff0000);
+  this.physics.add.existing(this.player);
   this.keys = this.input.keyboard.addKeys('W,A,S,D');
 
   this.cameras.main.startFollow(this.player);
   this.cameras.main.setBounds(0, 0, 2400, 1728);
 
-  
+this.walls = this.physics.add.staticGroup();
+this.walls.add(this.add.rectangle(200, 200, 64, 64, 0x8B4513));
+this.physics.add.collider(this.player, this.walls);
+
+this.physics.world.setBounds(0, 0, 2400, 1728);
+this.player.body.setCollideWorldBounds(true);
+
 }
 update() {
-const velocidad = 3;
-
-if (this.keys.W.isDown) {
-  this.player.y -= velocidad;
-}
 
 
-if (this.keys.A.isDown) {
-  this.player.x -= velocidad;
-}
+this.player.body.setVelocity(0, 0);
 
-if (this.keys.S.isDown) {
-  this.player.y += velocidad;
-}
-
-if (this.keys.D.isDown) {
-  this.player.x += velocidad;
-}
-
-if (this.player.x < 16) this.player.x = 16;
-if (this.player.x > 2384) this.player.x = 2384;
-if (this.player.y < 16) this.player.y = 16;
-if (this.player.y > 1712) this.player.y = 1712;
+if (this.keys.W.isDown) this.player.body.setVelocityY(-150);
+if (this.keys.S.isDown) this.player.body.setVelocityY(150);
+if (this.keys.A.isDown) this.player.body.setVelocityX(-150);
+if (this.keys.D.isDown) this.player.body.setVelocityX(150);
 
 }
 }
