a88ce8810c3a8362bca38b71ea2c90ad9b25759d
Sergio Ruiz <sergio.ruizluengo@gmail.com>
2026-05-15 12:40:56 +0200

    feat: cámara que sigue al jugador

--- src/scenes/GameScene.js
+++ src/scenes/GameScene.js
@@ -8,8 +8,8 @@ export default class GameScene extends Phaser.Scene {
     const graphics = this.add.graphics();
     
 
-    for(let fila = 0; fila < 18; fila++){
-      for(let columna = 0; columna < 25; columna++){
+    for(let fila = 0; fila < 54; fila++){
+      for(let columna = 0; columna < 75; columna++){
         graphics.fillStyle(0x228B22);
         graphics.fillRect(columna * 32, fila * 32, 32, 32);
       }
@@ -17,6 +17,11 @@ export default class GameScene extends Phaser.Scene {
     }
   this.player = this.add.rectangle(400, 300, 32, 32, 0xff0000);  
   this.keys = this.input.keyboard.addKeys('W,A,S,D');
+
+  this.cameras.main.startFollow(this.player);
+  this.cameras.main.setBounds(0, 0, 2400, 1728);
+
+  
 }
 update() {
 const velocidad = 3;
@@ -39,9 +44,9 @@ if (this.keys.D.isDown) {
 }
 
 if (this.player.x < 16) this.player.x = 16;
-if (this.player.x > 784) this.player.x = 784;
+if (this.player.x > 2384) this.player.x = 2384;
 if (this.player.y < 16) this.player.y = 16;
-if (this.player.y > 560) this.player.y = 560;
+if (this.player.y > 1712) this.player.y = 1712;
 
 }
 }
