65b761c64311de3041abfdd1c58b71559aea52c2
Sergio Ruiz <sergio.ruizluengo@gmail.com>
2026-05-18 11:16:57 +0200

    feat: tilemap con tiles de Kenney Desert Shooter

---
+++ src/assets/desert.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.1" name="desert" tilewidth="16" tileheight="16" tilecount="234" columns="18">
+ <image source="kenney_desert-shooter-pack_1.0/PNG/Tiles/Tilemap/tilemap_packed.png" width="288" height="208"/>
+</tileset>
--- src/scenes/GameScene.js
+++ src/scenes/GameScene.js
@@ -5,35 +5,28 @@ export default class GameScene extends Phaser.Scene {
   }
 
   create() {
-  const graphics = this.add.graphics();
 
-  for (let fila = 0; fila < 54; fila++) {
-    for (let columna = 0; columna < 75; columna++) {
-      graphics.fillStyle(0x228B22);
-      graphics.fillRect(columna * 32, fila * 32, 32, 32);
-    }
-  }
+  const map = this.make.tilemap({ key: 'map' });
+  const tileset = map.addTilesetImage('desert', 'tiles');
+  const layer = map.createLayer('Capa de patrones 1', tileset, 0, 0);
+  layer.setScale(2);
 
   this.player = this.add.rectangle(400, 300, 32, 32, 0xff0000);
   this.physics.add.existing(this.player);
   this.keys = this.input.keyboard.addKeys('W,A,S,D');
 
+  this.cameras.main.setBounds(0, 0, map.widthInPixels * 2, map.heightInPixels * 2);
+  this.physics.world.setBounds(0, 0, map.widthInPixels * 2, map.heightInPixels * 2);
+
+  this.player.body.setCollideWorldBounds(true);
   this.cameras.main.startFollow(this.player);
-  this.cameras.main.setBounds(0, 0, 2400, 1728);
-
-this.walls = this.physics.add.staticGroup();
-this.walls.add(this.add.rectangle(200, 200, 64, 64, 0x8B4513));
-this.physics.add.collider(this.player, this.walls);
-this.walls.add(this.add.rectangle(500, 400, 64, 64, 0x8B4513));
-this.walls.add(this.add.rectangle(800, 150, 64, 64, 0x8B4513));
-this.walls.add(this.add.rectangle(1200, 600, 64, 64, 0x8B4513));
-this.walls.add(this.add.rectangle(300, 900, 64, 64, 0x8B4513));
-this.walls.add(this.add.rectangle(1600, 300, 64, 64, 0x8B4513));
-
-this.physics.world.setBounds(0, 0, 2400, 1728);
-this.player.body.setCollideWorldBounds(true);
-
 }
+
+preload() {
+  this.load.image('tiles', 'assets/tilemap_packed.png');
+  this.load.tilemapTiledJSON('map', 'assets/map.json');
+}
+
 update() {
 
 
@@ -45,4 +38,5 @@ if (this.keys.A.isDown) this.player.body.setVelocityX(-150);
 if (this.keys.D.isDown) this.player.body.setVelocityX(150);
 
 }
+
 }
